69eeb18f878219b1fe6e1a97dde8da7633549fc6
Bhavik Mehta <bhavikmehta8@gmail.com>
2023-06-24 14:33:00 +0100

    triangle

--- src/physics/almost_periodicity.lean
+++ src/physics/almost_periodicity.lean
@@ -25,14 +25,56 @@ lemma lemma28 {ε : ℝ} {m : ℕ} {A : finset G} {f : G → ℂ} {k : ℕ}
   A.card ^ k / 2 ≤ (L k m ε f A).card :=
 sorry
 
-lemma just_the_triangle_inequality {ε : ℝ} {m : ℕ} {A : finset G} {f : G → ℂ} {k : ℕ} {t : G}
-  {a : fin k → G} (ha : a ∈ L k m ε f A) (ha' : a + (λ _, t) ∈ L k m ε f A) :
-  ‖τ t (mu A ∗ f) - mu A ∗ f‖_[2 * m] ≤ 2 * ε * ‖f‖_[2 * m] :=
-sorry
+-- proved elsewhere soon
+lemma lpnorm_sub_comm {α : Type*} [fintype α] {p : ennreal} {f g : α → ℂ} (hp : 1 ≤ p) :
+  ‖f - g‖_[p] = ‖g - f‖_[p] := sorry
+lemma lpnorm_smul {α : Type*} [fintype α] {p : ennreal} {k : ℝ} {f : α → ℂ} (hp : 1 ≤ p) :
+  ‖k • f‖_[p] = k • ‖f‖_[p] := sorry
+lemma lpnorm_nsmul {α : Type*} [fintype α] {p : ennreal} {k : ℕ} {f : α → ℂ} (hp : 1 ≤ p) :
+  ‖k • f‖_[p] = k • ‖f‖_[p] := sorry
+lemma nsmul_translate {α : Type*} [add_comm_group α] {k : ℕ} {t : α} {f : α → ℂ} :
+  k • τ t f = τ t (k • f) := sorry
+lemma lpnorm_sub_le {α : Type*} [fintype α] {p : ennreal} (f g h : α → ℂ) (hp : 1 ≤ p) :
+  ‖f - h‖_[p] ≤ ‖f - g‖_[p] + ‖g - h‖_[p] := sorry
 
-lemma big_shifts {k : ℕ} {A S : finset G} (L : finset (fin k → G)) :
-  L.card * S.card ≤
-    (A + S).card ^ k * (finset.univ.filter (λ t : G, ∃ a ∈ L, a + (λ _, t) ∈ L)).card :=
+lemma just_the_triangle_inequality {ε : ℝ} {m : ℕ} {A : finset G} {f : G → ℂ} {k : ℕ} {t : G}
+  {a : fin k → G} (ha : a ∈ L k m ε f A) (ha' : a + (λ _, t) ∈ L k m ε f A) (hk : 0 < k)
+  (hm : 1 ≤ m) :
+  ‖τ (-t) (mu A ∗ f) - mu A ∗ f‖_[2 * m] ≤ 2 * ε * ‖f‖_[2 * m] :=
+begin
+  let f₁ : G → ℂ := λ x, ∑ i, f (x - a i),
+  let f₂ : G → ℂ := λ x, ∑ i, f (x - a i - t),
+  have hp : (1 : ennreal) ≤ 2 * m := by norm_cast; linarith,
+  have h₁ : ‖f₁ - k • (mu A ∗ f)‖_[2*m] ≤ k * ε * ‖f‖_[2 * m],
+  { rw [L, finset.mem_filter] at ha, exact ha.2 },
+  have h₂ : ‖f₂ - k • (mu A ∗ f)‖_[2*m] ≤ k * ε * ‖f‖_[2 * m],
+  { rw [L, finset.mem_filter, L_prop] at ha',
+    refine ha'.2.trans_eq' _,
+    congr' with i : 1,
+    simp [f₂, sub_sub] },
+  have h₃ : f₂ = τ t f₁,
+  { ext i : 1,
+    rw translate_apply,
+    refine finset.sum_congr rfl (λ j hj, _),
+    rw sub_right_comm },
+  have h₄₁ : ‖τ t f₁ - k • (mu A ∗ f)‖_[2 * m] = ‖τ (-t) (τ t f₁ - k • (mu A ∗ f))‖_[2 * m],
+  { rw Lpnorm_translate },
+  have h₄ : ‖τ t f₁ - k • (mu A ∗ f)‖_[2 * m] = ‖f₁ - τ (-t) (k • (mu A ∗ f))‖_[2 * m],
+  { rw [h₄₁, translate_sub_right, translate_translate],
+    simp },
+  have h₅₁ : ‖τ (-t) (k • (mu A ∗ f)) - f₁‖_[2 * m] ≤ k * ε * ‖f‖_[2 * m],
+  { rwa [lpnorm_sub_comm hp, ←h₄, ←h₃] },
+  have : (0 : ℝ) < k, by positivity,
+  refine le_of_mul_le_mul_left _ this,
+  rw [←nsmul_eq_mul, ←lpnorm_nsmul hp, nsmul_sub, nsmul_translate, mul_assoc, mul_left_comm,
+    two_mul ((k : ℝ) * _), ←mul_assoc],
+  exact (lpnorm_sub_le _ _ _ hp).trans (add_le_add h₅₁ h₁),
+end
+
+#exit
+
+lemma big_shifts {k : ℕ} {A S : finset G} (L : finset (fin k → G)) (a : fin k → G) (ha : a ∈ L) :
+  L.card * S.card ≤ (A + S).card ^ k * (finset.univ.filter (λ t : G, a + (λ _, t) ∈ L)).card :=
 sorry
 
 -- trivially true for other reasons for big ε
