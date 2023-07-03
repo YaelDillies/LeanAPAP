67d32f067779f77a70e1afd0f856a614948eec2e
TFBloom <96446181+TFBloom@users.noreply.github.com>
2023-07-03 21:53:11 +0100

    Reverted tasks.py

--- blueprint/tasks.py
+++ blueprint/tasks.py
@@ -1,4 +1,5 @@
 import os
+import random
 from pathlib import Path
 import http.server
 import socketserver
@@ -8,7 +9,7 @@ from invoke import run, task
 BP_DIR = Path(__file__).parent
 
 @task
-def print(ctx):
+def print_bp(ctx):
     cwd = os.getcwd()
     os.chdir(BP_DIR)
     run('mkdir -p print && cd src && xelatex -output-directory=../print print.tex')
@@ -22,7 +23,6 @@ def bp(ctx):
     run('cd src && xelatex -output-directory=../print print.tex')
     os.chdir(cwd)
 
-
 @task
 def web(ctx):
     cwd = os.getcwd()
@@ -31,10 +31,15 @@ def web(ctx):
     os.chdir(cwd)
 
 @task
-def serve(ctx):
+def serve(ctx, random_port=False):
     cwd = os.getcwd()
     os.chdir(BP_DIR/'web')
     Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("", 8000), Handler)
+    if random_port:
+        port = random.randint(8000, 8100)
+        print("Serving on port " + str(port))
+    else:
+        port = 8000
+    httpd = socketserver.TCPServer(("", port), Handler)
     httpd.serve_forever()
     os.chdir(cwd)
