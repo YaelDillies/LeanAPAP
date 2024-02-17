eed7ce480b50a93dc64a6435b5da4d3f15d37a0c
Yaël Dillies <yael.dillies@gmail.com>
2024-02-17 18:27:08 +0000

    progress

--- LeanAPAP/MWE.lean
+++ LeanAPAP/MWE.lean
@@ -6,14 +6,12 @@ open Lean Elab Tactic Term Meta PrettyPrinter Qq
 def isFun {u : Level} (π : Q(Type $u)) : MetaM (Option (Σ v w : Level, Q(Type v) × Q(Type w))) := do
   let v ← mkFreshLevelMVar
   let w ← mkFreshLevelMVar
-  withNewMCtxDepth
-    (do
-      let α ← mkFreshExprMVar (some (.sort (.succ v)))
-      let β ← mkFreshExprMVar (some (.sort (.succ w)))
-      match ← withReducible (isDefEq (.forallE .anonymous α β .default) π) with
-      | true => pure (some ⟨v, w, ← instantiateMVars α, ← instantiateMVars β⟩)
-      | false => pure none)
-    true
+  withNewMCtxDepth (allowLevelAssignments := true) do
+    let α ← mkFreshExprMVar (some (.sort (.succ v)))
+    let β ← mkFreshExprMVar (some (.sort (.succ w)))
+    match ← withReducible (isDefEq (.forallE .anonymous α β .default) π) with
+    | true => pure (some ⟨v, w, ← instantiateMVars α, ← instantiateMVars β⟩)
+    | false => pure none
 
 end IsFun
 
@@ -38,16 +36,35 @@ open Lean Meta Qq
   let .some ⟨v, w, ~q($α), ~q($β)⟩ ← isFun π | throwError "not `conv`"
   have : u =QL max v w := ⟨⟩
   have : $π =Q ($α → $β) := ⟨⟩
-  match e with
+  match q($e) with
+  | ~q(@conv $α $β $f $g) => do
+    let _ ← synthInstanceQ q(OrderedAddCommMonoid $β)
+    let _ ← synthInstanceQ q(PartialOrder $β)
+    match ← core q(inferInstance) q(inferInstance) f with
+    | .positive rf => do
+      match ← core q(inferInstance) q(inferInstance) g with
+      | .positive rg => do
+        let rf : Q(0 < $f) := rf
+        let rg : Q(0 < $g) := rg
 /-
-don't know how to synthesize placeholder
-context:
-case m
-«$$v»«$$w»: Level
-$$x✝¹$$x✝: Expr × Bool
-⊢ Sort ?u.6232
+application type mismatch
+  conv_pos «$rf»
+argument
+  «$rf»
+has type
+  @OfNat.ofNat («$α» → «$β») 0
+      (@Zero.toOfNat0 («$α» → «$β») (@Pi.instZero «$α» (fun a ↦ «$β») fun i ↦ AddMonoid.toZero)) <
+    «$f» : Prop
+but is expected to have type
+  @OfNat.ofNat («$α» → «$β») 0
+      (@Zero.toOfNat0 («$α» → «$β») (@Pi.instZero «$α» (fun a ↦ «$β») fun i ↦ AddMonoid.toZero)) <
+    ?m.21991 : Prop
 -/
-  | ~q(@conv $α $β ($f : Q($α → $β)) ($g : Q($α → $β))) => _
+        return .positive q(@conv_pos $α $β _ _ _ $rf $rg)
+      | .nonnegative rg => sorry
+      | _ => return .none
+    | .nonnegative rf => sorry
+    | _ => return .none
   | _ => throwError "not `conv`"
 
 end Mathlib.Meta.Positivity
